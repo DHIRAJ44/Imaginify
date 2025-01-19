521ddcc7ee9eb39241628a843d802d03cd83d6ba
dhiraj Bagad <dhirajbagad03@gmail.com>
2025-01-20 00:14:19 +0530

    Changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,14 @@
 // import { Collection } from '@/components/shared/Collection'
-import { navLinks } from '@/constants'
+// import { navLinks } from '@/constants'
 // import { getAllImages } from '@/lib/actions/image.actions'
-import Image from 'next/image'
-import Link from 'next/link'
+// import Image from 'next/image'
+// import Link from 'next/link'
 import React from 'react'
 
 async function Home() {
 
-    return (<>
+    return (
+        <>
         </>
     )
 }
