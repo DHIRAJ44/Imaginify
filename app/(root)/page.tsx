ccaaac1f9213826cc52353789012f95d871e5908
dhiraj Bagad <dhirajbagad03@gmail.com>
2025-01-19 23:57:49 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+// import { Collection } from '@/components/shared/Collection'
+import { navLinks } from '@/constants'
+// import { getAllImages } from '@/lib/actions/image.actions'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+async function Home() {
+
+    return (<>
+        </>
+    )
+}
+
+export default Home
